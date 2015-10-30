f4dfa510e32382b0fcc3a4c53e8e7ad8cc6fbcf2
ambirdsall <ambirdsall@gmail.com>
2015-10-30 01:20:19 -0700

    Add tests for search/types/source

---
+++ test/source.spec.js
@@ -0,0 +1,47 @@
+var Source  = require('../search/js/types/source'),
+    extend  = require('../search/js/modules/utils').extend;
+
+describe('A source', function() {
+  var foo = {
+        type:     'foo',
+        mainCite: 'some',
+        jumpCite: ' stuff',
+        fullCite: 'some stuff'
+      },
+      generic = {
+        name:        'test_source',
+        baseUrl:     'http://butts.gov/',
+        canDeepLink: true,
+        anchor:      'a jquery object',
+        cannot:      ['the_truth']
+      };
+
+  it('can generate a formatted url with jump cite', function() {
+    var canDeepLink = new Source(generic);
+
+    expect(canDeepLink.url(foo)).toBe('http://butts.gov/some%20stuff');
+  });
+
+  it('can generate a formatted url without a jump cite', function() {
+    var cannotDeepLink = extend(Object.create(generic), {canDeepLink: false})
+    var onlySome = new Source(cannotDeepLink);
+
+    expect(onlySome.url(foo)).toBe('http://butts.gov/some');
+  });
+
+  it('can apply type-specific url formats', function() {
+    var qux    = extend(Object.create(foo), {type: 'qux'}),
+        tst    = {typeSpecificTreatments: {qux: function() { return 'something different'; }}}
+        typist = new Source(extend(Object.create(generic), tst));
+
+    expect(typist.url(foo)).toBe('http://butts.gov/some%20stuff');
+    expect(typist.url(qux)).toBe('something different');
+  });
+
+  it('can opt out of specific types', function() {
+    var kaffee = new Source(generic);
+
+    expect(kaffee.canHandle('the_truth')).toBe(false);
+    expect(kaffee.canHandle('foo')).toBe(true);
+  });
+});
