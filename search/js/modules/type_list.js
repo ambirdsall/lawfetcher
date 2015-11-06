689055e13ffd614e1054d60efa810c779f2d5256
ambirdsall <ambirdsall@gmail.com>
2015-11-05 16:35:05 -0800

    Properly indent federal rule subtypes is type_list.js

--- search/js/modules/type_list.js
+++ search/js/modules/type_list.js
@@ -11,50 +11,50 @@ module.exports = [
   },
   {
     name:            "usc",
-    idPattern:       /(?:U\.? ?S\.? ?C\.?)/i,
-    mainCitePattern: /([^\(]+)(?:\s*\(.\))+/
+    idpattern:       /(?:u\.? ?s\.? ?c\.?)/i,
+    maincitepattern: /([^\(]+)(?:\s*\(.\))+/
   },
   {
-    name: "frap",
-    idPattern: /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:appellate|app|a)/i,
-    mainCitePattern: /([^(]+)(?:s*(.))+/
+    name:            "frap",
+    idpattern:       /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:appellate|app|a)/i,
+    maincitepattern: /([^(]+)(?:s*(.))+/
   },
   {
-    name: "frcrmp",
-    idPattern: /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:criminal|crim|cr)/i,
-    mainCitePattern: /([^(]+)(?:s*(.))+/
+    name:            "frcrmp",
+    idpattern:       /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:criminal|crim|cr)/i,
+    maincitepattern: /([^(]+)(?:s*(.))+/
   },
   {
-    name: "frcp",
-    idPattern: /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:civil|civ|c)/i,
-    mainCitePattern: /([^(]+)(?:s*(.))+/
+    name:            "frcp",
+    idpattern:       /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:civil|civ|c)/i,
+    maincitepattern: /([^(]+)(?:s*(.))+/
   },
   {
-    name: "fre",
-    idPattern: /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:evidence|evid|e)/i,
-    mainCitePattern: /([^(]+)(?:s*(.))+/
+    name:            "fre",
+    idpattern:       /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:evidence|evid|e)/i,
+    maincitepattern: /([^(]+)(?:s*(.))+/
   },
   {
-    name: "frbp",
-    idPattern: /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:bankruptcy|bankr|bkrtcy)/i,
-    mainCitePattern: /([^(]+)(?:s*(.))+/
+    name:            "frbp",
+    idpattern:       /(?:(?:federal|fed|f)\.? ?(?:rules?|r)\.? ?)(?:of )?(?:bankruptcy|bankr|bkrtcy)/i,
+    maincitepattern: /([^(]+)(?:s*(.))+/
   },
   {
     name:            "federal_case",
-    idPattern:       /\d{1,5} (?:U\.? ?S\.?|S\. ?Ct\.|F\.(?:Supp\.?)?(?:\dd)?) d{1,5}/i,
+    idpattern:       /\d{1,5} (?:u\.? ?s\.?|s\. ?ct\.|f\.(?:supp\.?)?(?:\dd)?) d{1,5}/i,
     // some federal case citations have, e.g. '(2006)' following jump cite
     // (if present): anything to do about it?
-    mainCitePattern: /(.+\d{1,5})(?:, ?\d{1,5})/
+    maincitepattern: /(.+\d{1,5})(?:, ?\d{1,5})/
   },
   {
     name:            "state_constitution",
-    idPattern:       /Const/i,
-    mainCitePattern: /(.*(?:section|\u00a7) ?[\d\.]+).+/i
+    idpattern:       /const/i,
+    maincitepattern: /(.*(?:section|\u00a7) ?[\d\.]+).+/i
   },
   {
     name:            "law_statute_code_rule",
-    idPattern:       /(?:laws?)|(?:stat\.?)|(?:code)|(?:regs\.?)|(?:rule)/i,
-    mainCitePattern: /([^\(]+)(?:\s*\(.\))+/
+    idpattern:       /(?:laws?)|(?:stat\.?)|(?:code)|(?:regs\.?)|(?:rule)/i,
+    maincitepattern: /([^\(]+)(?:\s*\(.\))+/
   },
   {
     name:            "default",
